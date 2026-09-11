a53e1cad3501d51a3889d7c881307aac93cd2851
EGOUDJOBI Peace Fiacre <fiacreegoudjobi@gmail.com>
2026-09-11 19:43:24 +0100

    Config de la CI updated6

--- benrover_mapping/test/test_map_published.py
+++ benrover_mapping/test/test_map_published.py
@@ -26,12 +26,12 @@ import unittest
 import launch
 import launch_ros.actions
 import launch_testing.actions
-import launch_testing.markers
 from ament_index_python.packages import get_package_share_directory
 from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import OccupancyGrid
 import pytest
 import rclpy
+import launch_testing.markers
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from tf2_ros import StaticTransformBroadcaster
